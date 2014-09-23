ec0247e2f2aa49a9a1be8a00baa23b4456d33c8c
littleyang <littleyangqh@gmail.com>
2014-09-23 22:14:29 +0800

    add dialog ui set message and title

---
+++ app/src/main/java/com/example/yangyang/loginapp/ChoiceDialogFragment.java
@@ -0,0 +1,15 @@
+package com.example.yangyang.loginapp;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by yangyang on 14-9-23.
+ */
+public class ChoiceDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/yangyang/loginapp/ListDialogFragment.java
+++ app/src/main/java/com/example/yangyang/loginapp/ListDialogFragment.java
@@ -1,7 +1,33 @@
 package com.example.yangyang.loginapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
 /**
  * Created by yangyang on 14-9-23.
  */
-public class ListDialogFragment {
+public class ListDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // set title and message
+        builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+
+        // set ok and cancel button
+        builder.setPositiveButton(R.string.ok_button,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).setNegativeButton(R.string.cancle,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        return builder.create();
+    }
 }
