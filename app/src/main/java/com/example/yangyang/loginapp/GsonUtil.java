65644493ca5ca5bece18d8aa5239a86da188b568
littleyang <littleyangqh@gmail.com>
2014-11-18 22:00:55 +0800

    add gson implemntation

--- app/src/main/java/com/example/yangyang/loginapp/GsonUtil.java
+++ app/src/main/java/com/example/yangyang/loginapp/GsonUtil.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by yangyang on 11/18/14.
@@ -26,6 +27,13 @@ public class GsonUtil {
 
         Type listType = new TypeToken<ArrayList<UserModel>>(){}.getType();
         Gson gson = new Gson();
+        ArrayList<UserModel> userLists = gson.fromJson(jsonStr,listType);
+        Iterator iterator = userLists.iterator();
+        while (iterator.hasNext()){
+            UserModel user = (UserModel)iterator.next();
+            System.out.println("name === " + user.getName());
+            System.out.println("age === " + user.getAge());
+        }
 
     }
 }
